5adbc90649fd12e1b3f01b689d8d8587e25e133e
Hiroyuki Kuromiya <khiroyuki1993@gmail.com>
2021-07-05 17:39:17 +0900

    change module name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="evidenceportal",
+    name="tasks",
     version="0.1.0",
     author="hiroyuki-kuromiya",
     author_email="khiroyuki1993@gmail.com",
