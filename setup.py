713c2d3ad025951d29abea986fbb7675600b9dcf
Hiroyuki Kuromiya <khiroyuki1993@gmail.com>
2021-07-05 16:44:25 +0900

    use class

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sample_command",
+    name="ep_helper",
     version="0.1.0",
     author="hiroyuki-kuromiya",
     author_email="khiroyuki1993@gmail.com",
