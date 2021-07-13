72ae279375abac33b77958e13645366331b2d834
Hiroyuki Kuromiya <khiroyuki1993@gmail.com>
2021-07-13 18:16:50 +0900

    dynamic column name

--- src/evidenceportal/ep_helper.py
+++ src/evidenceportal/ep_helper.py
@@ -9,18 +9,22 @@ class API:
     def __init__(self, url):
         self.url = url
 
-    def prepost(self, df):
+    def prepost(self, df, col_pre, col_post):
         endpoint = self.url + "/prepost"
         payload = json.dumps({
-            "data": df[["pre", "post"]].to_dict(orient="records")
+            "data": df[["pre", "post"]].to_dict(orient="records"),
+            "col_pre": col_pre,
+            "col_post": col_post
         })
         response = requests.request("POST", endpoint, headers=self.headers, data=payload)
         return response.text
 
-    def anova(self, df):
+    def anova(self, df, col_group, col_y):
         endpoint = self.url + "/group"
         payload = json.dumps({
-            "data": df[["group", "y"]].to_dict(orient="records")
+            "data": df[["group", "y"]].to_dict(orient="records"),
+            "col_group": col_group,
+            "col_y": col_y
         })
         response = requests.request("POST", endpoint, headers=self.headers, data=payload)
         return response.text
